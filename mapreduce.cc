0ef22612d81ad315d503e9f304fcaf36251b83e8
mideipari <54919564+mideipari@users.noreply.github.com>
2020-11-01 19:50:40 -0600

    some setup

--- mapreduce.cc
+++ mapreduce.cc
@@ -3,16 +3,34 @@
  * Talib Pierson
  * 20 October 2020
  */
+#include <string>
+#include <queue>
+#include <future>
+#include <thread>
 #include "mapreduce.hh"
 
-void MapReduce::MR_Emit(const std::string &key, const std::string &value) {}
+int num_part;
+//Use part[partkey][key].emplace_back(value)
+using PART = std::vector<std::unordered_map<std::string, std::vector<std::string>>>;
+PART parts;
 
-unsigned long MapReduce::MR_DefaultHashPartition(const std::string &key,
-                                                 int num_partitions) {
-    std::hash<std::string> hash;
-    return hash(key) % num_partitions;
-}
 
 void MapReduce::MR_Run(int argc, char *argv[], MapReduce::mapper_t map,
                        int num_mappers, MapReduce::reducer_t reduce,
-                       int num_reducers, MapReduce::partitioner_t partition) {}
+                       int num_reducers, MapReduce::partitioner_t partition) {
+    std::queue<std::future<PART>> threadQueue;
+    num_part = num_reducers;
+    parts.resize(num_part);
+
+
+}
+void MapReduce::MR_Emit(const std::string &key, const std::string &value) {
+
+}
+
+unsigned long MapReduce::MR_DefaultHashPartition(const std::string &key,
+                                                 int num_partitions) {
+    return 0;
+}
+
+
