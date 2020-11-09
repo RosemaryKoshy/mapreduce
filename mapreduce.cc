7f8fae32792ee62f266ca9ca3b5be1338f2e952a
mideipari <54919564+mideipari@users.noreply.github.com>
2020-11-08 20:31:57 -0600

    Locked added

--- mapreduce.cc
+++ mapreduce.cc
@@ -14,15 +14,19 @@ using PART =
 std::vector<std::unordered_map<std::string, std::vector<std::string>>>;
 PART parts;
 std::queue<std::future<void>> threadQueue;
+std::mutex locked;
 
+//code to delete value, separated because it's complicated
 void deleteVal(const std::string &key, int part_num) {
+
     parts[part_num].at(key).erase(parts[part_num].at(key).begin());
 }
 
+//Returns the hash key from hash function
 unsigned long partitioner(const std::string &key, int num_partitions) {
     return MR_DefaultHashPartition(key, num_partitions);
 }
-
+//gets the value and deletes it from vector inside the map
 std::string getter(const std::string &key, int part_num) {
     if (!parts[part_num].at(key).empty()) {
         std::string value = parts[part_num].at(key).front();
@@ -44,8 +48,10 @@ std::string getter(const std::string &key, int part_num) {
 void MapReduce::MR_Run(int argc, char *argv[], MapReduce::mapper_t map,
                        int num_mappers, MapReduce::reducer_t reduce,
                        int num_reducers, MapReduce::partitioner_t partition) {
+    //set vector size correctly so we can put in keys
     num_part = num_reducers;
     parts.resize(num_reducers);
+    //loops through files and adds map function to thread queue, if threadqueue is bigger than num_maps pop and then continue.
     for(int i = 1; i < argc; i++){
         if ((int)threadQueue.size() >= num_mappers){
             threadQueue.front().get();
@@ -54,18 +60,19 @@ void MapReduce::MR_Run(int argc, char *argv[], MapReduce::mapper_t map,
         threadQueue.emplace(std::async(std::launch::async, map, argv[i]));
 
     }
+    //Run rest of map
     while(!threadQueue.empty()){
         threadQueue.front().get();
         threadQueue.pop();
     }
-
+    //We need the keys so we get them here
     std::vector<std::string> keys;
     for(int i = 0; i < (int)parts.size(); i++) {
         for (auto key: parts[i]) {
             keys.emplace_back(key.first);
         }
     }
-
+    //For each key run reduce
     for(int i = 0; i < (int)keys.size(); i++){
         if((int)threadQueue.size() >= num_reducers){
             threadQueue.front().get();
@@ -83,6 +90,7 @@ void MapReduce::MR_Run(int argc, char *argv[], MapReduce::mapper_t map,
 
 void MapReduce::MR_Emit(const std::string &key, const std::string &value) {
     unsigned long partKey = partitioner(key, num_part);
+    std::lock_guard<std::mutex> lock(locked);
     int exists = parts[partKey].count(key);
     if (exists > 0){
         parts[partKey][key].emplace_back(value);
